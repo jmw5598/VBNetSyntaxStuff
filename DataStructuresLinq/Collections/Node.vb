d4c9718133b3993d0b0fcfce699e3bade1a260fd
Jason White <jmw5598@gmail.com>
2019-07-26 20:46:55 -0400

    added New constructor

--- DataStructuresLinq/Collections/Node.vb
+++ DataStructuresLinq/Collections/Node.vb
@@ -2,5 +2,9 @@ Namespace Collections
     Public Class Node(Of T)
         Public Property Value As T
         Public Property NextNode As Node(Of T)
+
+        Public Sub New(ByVal value As T)
+            Me.Value = value
+        End Sub
     End Class
 End Namespace
