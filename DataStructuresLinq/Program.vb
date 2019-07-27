865fda60a99a9af6c737e79f2bcb4ddfa9b3d941
Jason White <jmw5598@gmail.com>
2019-07-26 20:47:14 -0400

    updated Program.vb to demo Collections

--- DataStructuresLinq/Program.vb
+++ DataStructuresLinq/Program.vb
@@ -1,6 +1,8 @@
 Imports System
 Imports System.Linq
 
+Imports DataStructuresLinq.Collections
+
 Module Program
 
     Public Sub Main(args As String())
@@ -21,6 +23,57 @@ Module Program
 
         ' Array maniuplation
         Console.WriteLine()
+
+
+        ' Using my generic LinkedList
+        Dim list As IList(Of Integer) = new LinkedList(Of Integer)()
+        list.Add(4)
+        list.Add(1)
+        list.Add(2)
+        list.Add(5)
+
+        Console.WriteLine("LinkedList Count: " & list.Count())
+
+        Dim index As Integer = list.IndexOf(5)
+        Console.WriteLine("Index of 5 = " & index)
+        Console.WriteLine("value At Index " & index & ": " & list.GetValue(index))
+
+
+        Console.WriteLine()
+        
+        ' Using my generic Stack
+        Dim stack As IStack(Of Integer) = new Stack(Of Integer)()
+        stack.Push(5)
+        stack.Push(3)
+        stack.Push(12)
+        stack.Push(9)
+
+        Console.WriteLine("Stack Count: " & stack.Count())
+        Console.WriteLine("Popping 3 ....")
+        stack.Pop()
+        stack.Pop()
+        stack.Pop()
+
+        Console.WriteLine("Stack Peek: " & stack.Peek())
+        Console.WriteLine("New Stack Count: " & stack.Count())
+
+        Console.WriteLine()
+
+        Dim queue AS IQueue(Of Integer) = new Queue(Of Integer)()
+        queue.Enqueue(1)
+        queue.Enqueue(23)
+        queue.Enqueue(82)
+        queue.Enqueue(55)
+
+        Console.WriteLine("Queue Count: " & queue.Count())
+        Console.WriteLine("Dequeueing 3 ....")
+        queue.Dequeue()
+        queue.Dequeue()
+        queue.Dequeue()
+
+        Console.WriteLine("Queue Peek: " & queue.Peek())
+        Console.WriteLine("New Queue Count: " & queue.Count())
+
     End Sub
 
     ' Reverse a String with Linq
